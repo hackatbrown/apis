348afbe94f80799f99d3c8aa48ecd935aa354099
joengelm <joseph_engelman@brown.edu>
2015-01-06 00:11:41 -0500

    Re-organized folders and created dining API structure

---
+++ api/dining.py
@@ -0,0 +1,51 @@
+from flask import request
+from api import app
+
+from datetime import datetime
+
+@app.route('/dining/menu')
+def req_dining_menu():
+	eatery = request.args.get('eatery')
+	now = datetime.now()	# use current datetime as default
+	year = request.args.get('year', now.year)
+	month = request.args.get('month', now.month)
+	day = request.args.get('day', now.day)
+	hour = request.args.get('hour', now.hour)
+	minute = request.args.get('minute', now.minute)
+
+	# TODO find menu for eatery on date at time
+
+@app.route('/dining/hours')
+def req_dining_hours():
+	eatery = request.args.get('eatery')
+	now = datetime.now()	# use current date as default
+	year = request.args.get('year', now.year)
+	month = request.args.get('month', now.month)
+	day = request.args.get('month', now.day)
+
+	# TODO find hours for eatery on date
+
+@app.route('/dining/find')
+def req_dining_find():
+	food = request.args.get('food', None)
+
+	# TODO finding eatery serving food
+
+@app.route('/dining/nutrition')
+def req_dining_nutrition():
+	food = request.args.get('food', None)
+
+	# TODO find nutritional info for food
+
+@app.route('/dining/open_eateries')
+def req_dining_open_eateries():
+	now = datetime.now()	# use current datetime as default
+	year = request.args.get('year', now.year)
+	month = request.args.get('month', now.month)
+	day = request.args.get('day', now.day)
+	hour = request.args.get('hour', now.hour)
+	minute = request.args.get('minute', now.minute)
+
+	# TODO find open eateries
+
+
