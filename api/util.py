4aeee96d9625ea73a976f527df0f081423d2d37b
Justin Brower <justin_brower@brown.edu>
2015-01-11 19:44:14 -0500

    Fixed getDiningDate() to actually work

--- api/util.py
+++ api/util.py
@@ -1,12 +1,13 @@
 #This file contains some methods for dealing with Brown's 
 #dining date system.
 
+import datetime
 from datetime import date
 
 def getDiningDate():
 	'''Returns the current dining date as a date object. This extends until 2am the next morning.'''
 	today_date = date.today()
-	today_dtime = datetime.today()
+	today_dtime = datetime.now()
 	#if it's before 2am, paxson doesn't consider it a new dining day yet
 	if (today_dtime.hour < 2):
 		return (today_date - timedelta(1))
