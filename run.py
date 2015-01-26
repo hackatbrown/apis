780f181bcbb6a16d39dd0e45e0e7fc49e4fae6aa
joengelm <joseph_engelman@brown.edu>
2015-01-26 10:21:23 -0500

    Fixed imports in run.py

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from api import app
+from os import environ
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(environ.get("PORT", 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
