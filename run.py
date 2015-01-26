bc2c4ee7909a38c79a7d2b9cc3b6677ee5a714b2
joengelm <joseph_engelman@brown.edu>
2015-01-26 10:18:44 -0500

    Fixed indentation error

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from api import app
 
 if __name__ == '__main__':
-	port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
