f06de5b514aaf94449056a8a0368ca53fbcbfad4
joengelm <joseph_engelman@brown.edu>
2015-01-28 20:32:23 -0500

    Turned debug mode off temporarily

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from os import environ
 
 if __name__ == '__main__':
     port = int(environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=False, host='0.0.0.0', port=port)
