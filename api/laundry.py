bf9af892d27781494f895ef9fea9cacf47341a8c
Joe Stein <joeaarons@gmail.com>
2015-12-24 00:54:26 -0500

    [laundry] now uses require_client_id decorator

--- api/laundry.py
+++ api/laundry.py
@@ -1,41 +1,20 @@
 # vim: set ts=4 sts=4 sw=4 expandtab:
 from flask import request, jsonify
 from api import app, db, make_json_error, support_jsonp
-from api.meta import is_valid_client, log_client, INVALID_CLIENT_MSG
+from api.meta import require_client_id
 from api.scripts.laundry import Room
-from functools import wraps
-
-from datetime import datetime  # , date, timedelta
-# from difflib import get_close_matches
 
 '''
-DATABASE OBJECTS: View templates on the private, repository README.
-Nutritional info is not yet finished. Contact Joe for a preliminary template.
+DATABASE OBJECTS: View templates on the private repository README.
 '''
 
 # simplify database names
 ldb = db.laundry
 
 
-class requireKey(object):
-    def __init__(self, endpoint_name):
-        self.endpoint_name = endpoint_name
-
-    def __call__(self, f):
-        @wraps(f)
-        def wrapped_fn(*args, **kwargs):
-            client_id = request.args.get('client_id', 'missing_client')
-            if is_valid_client(client_id):
-                log_client(client_id, '/dining', str(datetime.now()))
-                return f(*args, **kwargs)
-            else:
-                return make_json_error(INVALID_CLIENT_MSG)
-        return wrapped_fn
-
-
 @app.route('/laundry')
 @support_jsonp
-@requireKey('/laundry')
+@require_client_id()
 def laundry_index():
     return make_json_error('No method specified. '
                            'See documentation for endpoints.')
@@ -43,7 +22,7 @@ def laundry_index():
 
 @app.route('/laundry/rooms')
 @support_jsonp
-@requireKey('/laundry/rooms')
+@require_client_id()
 def req_laundry_room_list():
     results = ldb.find({}, {'_id': False, 'machines': False})
     result_list = [r for r in results]
@@ -52,7 +31,7 @@ def req_laundry_room_list():
 
 @app.route('/laundry/rooms/<room_id>')
 @support_jsonp
-@requireKey('/laundry/rooms/<room_id>')
+@require_client_id()
 def req_room_detail(room_id):
     room = ldb.find_one({'id': str(room_id)}, {'_id': False})
     if room is None:
@@ -62,7 +41,7 @@ def req_room_detail(room_id):
 
 @app.route('/laundry/rooms/<room_id>/machines')
 @support_jsonp
-@requireKey('/laundry/rooms/<room_id>/machines')
+@require_client_id()
 def req_machines(room_id):
     # TODO make a type field to filter on (washer, dryer, etc)
 
@@ -81,7 +60,7 @@ def req_machines(room_id):
 
 @app.route('/laundry/rooms/<room_id>/machines/<machine_id>')
 @support_jsonp
-@requireKey('/laundry/rooms/<room_id>/machines/<machine_id>')
+@require_client_id()
 def req_machine_details(room_id, machine_id):
     # get_statuses = request.args.get('get_statuses')
 
