ce8d036b2c172ceceaf6eea65071148cbd454c9b
hpincket <harrison_pincket@brown.edu>
2016-01-25 10:51:09 -0500

    Abstracted out pagination

--- api/courses.py
+++ api/courses.py
@@ -4,6 +4,8 @@ from api.meta import is_valid_client, log_client, INVALID_CLIENT_MSG
 
 from datetime import datetime, date, timedelta
 
+import functools
+
 import json
 import urllib
 
@@ -14,52 +16,19 @@ from api.scripts.coursemodels import *
 
 connect('brown')
 
-DEFAULT_LIMIT = 10
+PAGINATION_LIMIT = 10
+PAGINATION_MAX = 42 #TODO: Make this do something.
 
 @app.route('/courses')
 @support_jsonp
 def courses_index():
-    '''
-    client_id = request.args.get('client_id', 'missing_client')
-    if is_valid_client(client_id):
-            log_client(client_id, '/courses', str(datetime.now()))
-    else:
-            return make_json_error(INVALID_CLIENT_MSG)
-    '''
-    offset = request.args.get('offset', '')
-    limit = int(request.args.get('limit', DEFAULT_LIMIT))
-    assert(limit > 0)
-    # total = len(BannerCourse.objects)
-    if offset != '':
-        res = BannerCourse.objects(id__gt=offset)[:limit+1]
-    else:
-        res = BannerCourse.objects()[:limit+1]
-    next_url = "null"
-    if len(res) == limit+1:
-        next_url = request.base_url + "?" + urllib.parse.urlencode({"limit": limit, "offset": res[limit- 1].id})
-    ans = {"href": request.url,
-            "items": [json.loads(elm.to_json()) for elm in res],
-            "limit": limit,
-            "next": next_url,
-            "offset": offset}
-    return jsonify(ans)
+    return jsonify(paginate({}))
 
 
 @app.route('/courses/<course_id>')
 @support_jsonp
 def course_specified(course_id):
     ''' Endpoint for all courses find requests (see public docs for documentation) '''
-    '''
-    client_id = request.args.get('client_id', 'missing_client')
-    if is_valid_client(client_id):
-            log_client(client_id, '/wifi/count', str(datetime.now()))
-    else:
-            return make_json_error(INVALID_CLIENT_MSG)
-    '''
-    offset = request.args.get('offset', '')
-    limit = int(request.args.get('limit', '1'))
-    assert(limit > 0)
-
     if "-" in course_id:
         # Particular section or lab specificied
         res = BannerCourse.objects(full_number=course_id).first()
@@ -67,19 +36,7 @@ def course_specified(course_id):
             return make_json_error("No section found.") #TODO: Standardize error messages
         return jsonify(json.loads(res.to_json()))
     else:
-        # Get all sections and labs
-        if offset != '':
-            res = BannerCourse.objects(number=course_id, id__gt=offset)[:limit+1]
-        else:
-            res = BannerCourse.objects(number=course_id)[:limit+1]
-        next_url = "null"
-        if len(res) == limit+1:
-            next_url = request.base_url + "?" + urllib.parse.urlencode({"limit": limit, "offset": res[limit - 1].id})
-        ans = {"href": request.url,
-                "items": [json.loads(elm.to_json()) for elm in res],
-                "limit": limit,
-                "next": next_url,
-                "offset": offset}
+        ans = paginate({"number":course_id})
         return jsonify(ans)
 
 @app.route('/instructors')
@@ -94,22 +51,7 @@ def instructors_index():
 @support_jsonp
 def instructors_specified(instructor_name):
     ''' Endpoint for a given instructor '''
-    # TODO: Query Semester Limitations, perhaps abstract this?
-    offset = request.args.get('offset', '')
-    limit = int(request.args.get('limit', DEFAULT_LIMIT))
-    if offset != '':
-        res = BannerCourse.objects(id__gt=offset, instructors__name=instructor_name)[:limit+1]
-    else:
-        res = BannerCourse.objects(instructors__name=instructor_name)[:limit+1]
-
-    next_url = "null"
-    if len(res) == limit+1:
-        next_url = request.base_url + "?" + urllib.parse.urlencode({"limit": limit, "offset": res[limit- 1].id})
-    ans = {"href": request.url,
-            "items": [json.loads(elm.to_json()) for elm in res],
-            "limit": limit,
-            "next": next_url,
-            "offset": offset}
+    ans = paginate({"instructors__name": instructor_name})
     return jsonify(ans)
 
 @app.route('/departments')
@@ -124,21 +66,7 @@ def departments_index():
 @support_jsonp
 def departments_specified(dept_code):
     ''' Endpoint for a given department '''
-    dept_code = dept_code.upper()
-    offset = request.args.get('offset', '')
-    limit = int(request.args.get('limit', DEFAULT_LIMIT))
-    if offset != '':
-        res = BannerCourse.objects(id__gt=offset, dept__code=dept_code)[:limit+1]
-    else:
-        res = BannerCourse.objects(dept__code=dept_code)[:limit+1]
-    next_url = "null"
-    if len(res) == limit+1:
-        next_url = request.base_url + "?" + urllib.parse.urlencode({"limit": limit, "offset": res[limit- 1].id})
-    ans = {"href": request.url,
-            "items": [json.loads(elm.to_json()) for elm in res],
-            "limit": limit,
-            "next": next_url,
-            "offset": offset}
+    ans = paginate({"dept__code":dept_code})
     return jsonify(ans)
 
 
@@ -149,8 +77,26 @@ def schedule_time():
     time = request.args.get('time','')
     return check_against_time(day, time, time)
 
+@app.route('/non-conflicting')
+@support_jsonp
+def non_conflicting():
+    course = request.args.get('courses','')
+    # MONGO QUERY: db.banner_course.find({meeting: {$all: [{$elemMatch: {"start_time": {$gte: 37200}}}]}})
+    print(course)
+    course_obj = BannerCourse.objects(full_number=course).first()
+    print(course_obj)
+    meetings = course_obj.meeting
+    query = []
+    f
+    query.append({"$elemMatch": {"day_of_week": {"$ne": meetings[0].day_of_week}}})
+    print(query)
+    #res = BannerCourse.objects().filter(Q(meeting__all__day_of_week__ne="R")])
+    res = BannerCourse.objects(__raw__={"meeting": {"$all": query}})
+    return jsonify(items=[json.loads(elm.to_json()) for elm in res])
 
-def check_against_time(concurrent=True, day, stime, etime):
+
+
+def check_against_time(day, stime, etime, concurrent=True):
     # print(time)
     # res = BannerCourse.objects(meeting__start_time__lte=time, meeting__end_time__gte=time, days_of_week=day)
     if concurrent:
@@ -164,6 +110,27 @@ def check_against_time(concurrent=True, day, stime, etime):
 # Helper methods
 
 # TODO add any helper methods (methods that might be useful for multiple endpoints) here
+
+def paginate(query_args):
+    offset = request.args.get('offset', '')
+    limit = int(request.args.get('limit', PAGINATION_LIMIT))
+    print(offset)
+    print(limit)
+    if offset != '':
+        res = BannerCourse.objects(id__gt=offset, **query_args)[:limit+1]
+    else:
+        res = BannerCourse.objects(**query_args)[:limit+1]
+
+    next_url = "null"
+    if len(res) == limit+1:
+        next_url = request.base_url + "?" + urllib.parse.urlencode({"limit": limit, "offset": res[limit- 1].id})
+    ans = {"href": request.url,
+            "items": [json.loads(elm.to_json()) for elm in res],
+            "limit": limit,
+            "next": next_url,
+            "offset": offset}
+    return ans
+
 
 
 def jsonify_mongoengine(json_data):
