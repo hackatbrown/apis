134af578afd1595f8f0c228222b875f3a509eb01
joengelm <joseph_engelman@brown.edu>
2016-04-07 18:23:05 -0400

    hotfix: disable debug mode

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # This file is for configuration variables that can be included in the Git repository.
 # This file should NOT contain secret keys, private URLs/URIs, or allow debugging.
 
-DEBUG = True
+DEBUG = False
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
