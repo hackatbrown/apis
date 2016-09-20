a1a8fca1629c258a09b7682b72ae21d8a35becdb
hpincket <harrison_pincket@brown.edu>
2016-09-19 23:03:39 -0400

    Revert "Revert "Merge pull request #62 from hpincket/master""
    In which harrison reverts joe's revert so that the merge is better.
    
    This reverts commit ee7b1ddf24cc1a2a7b96a5d6a770b9b4457513bb.

--- api/scripts/add_member.py
+++ api/scripts/add_member.py
@@ -2,11 +2,12 @@ from api import db
 
 api_members = db.members
 
+
 def add_member(about, name, imageurl):
     new_member = {
-            'name': name,
-            'about': about,
-            'image_url': imageurl
-            }
+        'name': name,
+        'about': about,
+        'image_url': imageurl
+    }
     api_members.insert(new_member)
     return True
--- api/scripts/enable_client.py
+++ api/scripts/enable_client.py
@@ -1,9 +1,9 @@
 from sys import argv
 
-from api import db, meta
+from api import meta
 
 if __name__ == '__main__':
-	if len(argv) != 2:
-		print("Usage: python -m api.scripts.enable_client <client_id>")
-	else:
-		print(meta.validate_client(argv[1]))
+    if len(argv) != 2:
+        print("Usage: python -m api.scripts.enable_client <client_id>")
+    else:
+        print(meta.validate_client(argv[1]))
